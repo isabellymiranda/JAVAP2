b7e6cc96807c5f2fe06322d6e9bcecba0ead1df6
Isabelly Miranda <contatoisabellymiranda@gmail.com>
2022-06-06 17:01:02 +0000

    Envio

---
+++ Questão1.java
@@ -0,0 +1,12 @@
+public class Questão1{
+    public static void main(String[] args) {
+        int segundaSemana = 5;
+        int terceiraSemana =  2;
+        int quartaSemana =  7;
+
+        System.out.println("Valores da semana:");
+        System.out.println("Segunda Semana: " + segundaSemana +".");
+        System.out.println("Terceira Semana: " + terceiraSemana + ".");
+        System.out.println("Quarta Semana: " + quartaSemana + ".");
+    }
+}
